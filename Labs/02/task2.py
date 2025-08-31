691a7c478d00f96a88c11a1ef28443f1a58c7526
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2025-08-31 22:23:34 +0500

    Update task2.py

--- Labs/02/task2.py
+++ Labs/02/task2.py
@@ -1,1 +1,33 @@
+import pandas as pd
+import numpy as np
+
+dfa = pd.read_csv(r"C:\Users\thisp\Downloads\Lab2 D1A (1).csv")
+dfb = pd.read_csv(r"C:\Users\thisp\Downloads\Lab2 D1B (1).csv")
+dfc = pd.read_csv(r"C:\Users\thisp\Downloads\Lab2 D1C (1).csv")
+dfa_names = dfa['name'].head(10).tolist()
+
+# Custom Dataset
+data = {
+    'name' : dfa_names, 'Size': np.random.choice(['small','medium','high'], size =10),
+    'Cardinal_Direction': np.random.choice(['North','South','East','West'], size=10),
+    'Timings': np.random.choice(['full time', 'part time'], size=10),
+    'Department': np.random.choice(['Science', 'Commerce', 'Humanities'], size=10),
+    'years_established': np.random.randint(5, 100, size=10)
+}
+
+customizedData = pd.DataFrame(data)
+print("Customized Dataset:")
+print(customizedData)
+print("\nShape of customizedData:", customizedData.shape)
+
+# Merge Custom Data
+# using 'left' to keep all the relevant data from the original dataframes
+updatedData = pd.merge(customizedData, dfa, on= 'name', how='left')
+updatedData = pd.merge(customizedData, dfb, on= 'name', how='left')
+updatedData = pd.merge(customizedData, dfc, on= 'name', how='left')
+
+print("\n Final Dataset: ")
+print(customizedData.head())
+print("\nShape of Customized Data: ",customizedData.shape)
+print("Columns included: ", customizedData.columns.tolist())
 
